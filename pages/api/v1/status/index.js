a6635da8a8f38058f93b69936e1f704909afdf52
Glayberson <113299484+Glayber16@users.noreply.github.com>
2025-10-24 00:17:18 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   response.status(200).json({ chave: "valor" });
+  const result = await database.query("SELECT 1 + 1 as RESULT;");
+  console.log(result.rows);
 }
 export default status;
