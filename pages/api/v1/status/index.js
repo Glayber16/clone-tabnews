afad01cb515ba84dd5e1ab5c4fa20bca141811a2
Glayberson <113299484+Glayber16@users.noreply.github.com>
2025-10-28 01:37:23 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,26 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
-  response.status(200).json({ chave: "valor" });
-  const result = await database.query("SELECT 1 + 1 as RESULT;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query("SHOW max_connections;");
+  const versionDb = await database.query("SHOW server_version;");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionDb.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        using_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 export default status;
